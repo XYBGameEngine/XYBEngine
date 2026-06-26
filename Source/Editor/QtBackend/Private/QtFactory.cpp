47d9cab8f769cd3e7995bae406082512716925f5
XYBEngine <xiangyubo18@gmail.com>
2026-06-27 00:23:47 +0800

    虚幻风格皮肤

--- Source/Editor/QtBackend/Private/QtFactory.cpp
+++ Source/Editor/QtBackend/Private/QtFactory.cpp
@@ -16,9 +16,12 @@ namespace XYBEngine
     {
         switch (type)
         {
+        case EditorWindowType::ApplicationWindow:
+            return MakeShared<QtApplication>();
         case EditorWindowType::ConsoleWindow:
             return MakeShared<ConsoleWindow>();
         default:
+            XYB_LOG_ERROR("Invalid editor window type");
             return nullptr;
         }
     }
