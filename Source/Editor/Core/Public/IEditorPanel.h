81df0df4fba4f86e0a025eaf7637b5890ae4d5c1
XYBEngine <xiangyubo18@gmail.com>
2026-06-28 14:40:32 +0800

    成功架构

--- Source/Editor/Core/Public/IEditorPanel.h
+++ Source/Editor/Core/Public/IEditorPanel.h
@@ -33,19 +33,6 @@ namespace XYBEngine
         void Register(const String& panelId, EditorPanelFactory editorPanelFactory);
         SharedPtr<IEditorPanel> CreateEditorPanel(const String& panelId);
     };
-
-    class XYB_API IEditorShell
-    {
-    public:
-        virtual ~IEditorShell() = default;
-        
-        virtual void Initialize() = 0;
-        virtual void Show() = 0;
-        virtual void Hide() = 0;
-        virtual void Close() = 0;
-        virtual void AddPanel(const String& defaultArea, SharedPtr<IEditorPanel> panel) = 0;
-        virtual void TogglePanel(const String& panelId) = 0;
-    };
 }
 
 #endif
